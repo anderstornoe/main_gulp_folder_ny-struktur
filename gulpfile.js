2a80bd1afb65f364a8a0a0cbdb3f89da9347aa7c
Anders Tornøe <ato@kvuc.dk>
2016-05-18 14:30:23 +0200

    no message

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,8 @@
          'bower_components/jquery-ui/jquery-ui.js',
          'bower_components/bootstrap/dist/js/bootstrap.js',
          'bower_components/jquery-ui-touch-punch/jquery.ui.touch-punch.js',
-         'bower_components/jquery-ui-sortable-animation/jquery.ui.sortable-animation.js'
+         'bower_components/jquery-ui-sortable-animation/jquery.ui.sortable-animation.js',
+         'bower_components/jquery.boxfit/dist/jquery.boxfit.js'
 
      ],
 
