37d0c8b03bdf5690b901da6bd9f73d804a044b50
Thomas <kvucthan@gmail.com>
2017-02-20 14:22:29 +0100

    Tvangscommit

--- objekter/development/library/custom_scripts.js
+++ objekter/development/library/custom_scripts.js
@@ -1381,3 +1381,89 @@ function getUrlVars() {
     (jQuery.browser = jQuery.browser || {}).mobile = /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)) })(navigator.userAgent || navigator.vendor || window.opera);
 
 /*=====  IS MOBILE BROWSER? ======*/
+
+
+
+/*======================================
+=            MicroHint 2017            =
+======================================*/
+
+ $('.ElementBox').click(function() {
+
+
+        var kvadrant = getPos($(this));
+
+        alert ("K: " + kvadrant);
+
+        $('.ElementBox').qtip({
+            content: '<b>Hvorfor bruger vi det?</b> Det er en kendsgerning, at man bliver distraheret af læsbart indhold på en side, når man betragter dens layout. Meningen med at bruge Lorem Ipsum er, at teksten indeholder mere eller mindre almindelig tekstopbygning i modsætning til "Tekst her – og mere tekst her", mens det samtidigt ligner almindelig tekst. Mange layoutprogrammer og webdesignere bruger Lorem Ipsum som fyldtekst. En søgning på Lorem Ipsum afslører mange websider, som stadig er på udviklingsstadiet. Der har været et utal af variationer, som er opstået enten på grund af fejl og andre gange med vilje (som blandt andet et resultat af humor).',
+            position: {
+                my: 'top left', // Position my top left...
+                at: 'bottom right', // at the bottom right of..
+            }   
+
+        });
+
+    });
+
+
+
+});
+
+
+function getPos(obj) {
+    /*var all = document.getElementsByTagName("*");
+    all.each(function(index) {
+        this.hide();
+    });*/
+
+    var container_height = $(".container-fluid").height();
+    var container_width = $(".container-fluid").width();
+
+
+    console.log("container_height: " + container_height + "  container_width: " + container_width);
+
+
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+
+    console.log("window_height: " + h + "  window_width: + w " + w);
+
+    var vertTopPosition = true;
+    var horzLeftPosition = true;
+
+    var kvadrant;
+
+    var p = obj;
+    p.css("border", "2px solid red");
+    var position = p.offset();
+    console.log("left: " + position.left + ", top: " + position.top);
+
+
+    if (position.top > container_height / 2) {
+        vertTopPosition = false;
+    }
+
+    if (position.left > container_width / 2) {
+        horzLeftPosition = false;
+    }
+
+    if (vertTopPosition && horzLeftPosition) {
+        kvadrant = 1;
+    } else if (vertTopPosition && !horzLeftPosition) {
+        kvadrant = 2;
+
+    } else if (!vertTopPosition && horzLeftPosition) {
+        kvadrant = 3;
+    } else {
+        kvadrant = 4;
+    }
+
+    return kvadrant;
+    //microhint(kvadrant);
+    console.log("KVADRANT = " + kvadrant);
+}
+
+
+/*=====  End of MicroHint 2017  ======*/
+
